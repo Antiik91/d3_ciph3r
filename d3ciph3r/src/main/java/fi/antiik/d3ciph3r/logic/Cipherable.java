a87a8f58817be3beca1950c977eaf0343af09d40
Antiik91 <antiik91@gmail.com>
2016-08-03 11:11:16 +0300

    redo the project, interface to cipherables

---
+++ d3ciph3r/src/main/java/fi/antiik/d3ciph3r/logic/Cipherable.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.antiik.d3ciph3r.logic;
+
+/**
+ *
+ * @author User
+ */
+public interface Cipherable {
+    public void encrypt();
+    public void decrypt();
+}
